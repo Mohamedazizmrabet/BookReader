a1f9e4befeb0985433dbd075dba641d84d5d420c
Mohamed Mrabet <81630459+Mohamedazizmrabet@users.noreply.github.com>
2024-03-09 22:15:29 +0100

    let's go

--- my-app/app/components/landing/index.tsx
+++ my-app/app/components/landing/index.tsx
@@ -4,14 +4,26 @@ import Image from 'next/image'
 function Landing() {
 
   return (
-    <div>
-        <div className='w-3/4 mx-auto'>
+    <div className="grid grid-cols-2 gap-10 items-center ml-32">  
+  <div className="col-span-1 w-full">
+    <h1 className="text-5xl font-bold w-full mt-9">
+      {upperCaseTheFirstLetter(
+        "Upload Your Books And Start Reading Them With Us. Invite Your friends And see their progress ."
+      )}
+    </h1>
+    <h2 className='text-2xl mt-9' >The most appropriate book site to read books.</h2>
+  </div>
+  <div className='bg-[#f0c384] rounded-full	w-2/4 h-3/4  mr-16'>
+  <Image
+    loading="lazy"
+    src="/photo-1604582480408-e7401f3c18bf-removebg-preview.png"
+    alt="books"
+    width={500}
+    height={500}
+  />
 
-          <h1 className="text-4xl font-bold w-2/3 mt-48  " >{upperCaseTheFirstLetter("upload Your Books And Start Reading Them With Us.  Invite Your friends And see there progress .")}</h1>
-        </div>
-        <Image src="https://images.unsplash.com/photo-1604582480408-e7401f3c18bf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="books" width={500} height={500}/>
-
-    </div>
+  </div>
+</div>
   )
 }
 
