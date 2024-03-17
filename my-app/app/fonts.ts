3355bcbb5048deab0979fb178eeb2f949e3c42fb
Mohamed Mrabet <81630459+Mohamedazizmrabet@users.noreply.github.com>
2024-03-17 14:41:05 +0100

    done

---
+++ my-app/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Poppins, Roboto_Mono } from 'next/font/google'
+ 
+export const poppins = Poppins({
+  subsets: ['latin'],
+  weight:"400",
+  display: 'swap',
+
+})
+ 
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { poppins } from './fonts'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" >
+      <head>
+        {/* Import the font styles here (optional for global usage) */}
+        <style>{`${poppins.style}`}</style>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
